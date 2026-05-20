bcfb46b9307f756328dbe996990a2f594ec0d995
khanhhx <hoangxuankhanh.work@gmail.com>
2026-05-20 14:38:55 +0700

    fix:deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,10 +32,7 @@ import { redisStore } from 'cache-manager-redis-yet';
       useFactory: async (configService: ConfigService) => ({
         store: await redisStore({
           socket: {
-            host:
-              configService.get<string>('REDIS_HOST') === 'redis'
-                ? 'localhost'
-                : configService.get<string>('REDIS_HOST') || 'localhost',
+            host: configService.get<string>('REDIS_HOST') || 'localhost',
             port: configService.get<number>('REDIS_PORT') || 6380,
           },
           password: configService.get<string>('REDIS_PASSWORD'),
